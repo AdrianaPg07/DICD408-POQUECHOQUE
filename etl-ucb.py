2ca8ea0f106c8e7cdcd8978b8472653cde7599cd
E.Adriana Poquechoque Gutierrez <84420348+AdrianaPg07@users.noreply.github.com>
2024-05-25 00:11:34 +0000

    cargar

--- etl-ucb.py
+++ etl-ucb.py
@@ -11,3 +11,8 @@ wine_quality_data=pd.read_csv(wine_quality_url,sep=";")
 print (wine_data.head())
 print(wine_quality_data.head())
 
+# TRANSFORMACION
+
+# cargar los datos....
+wine_data.to_csv('wine_dataset',index=False)
+wine_quality_data.to_csv('wine_quaily_dataset.csv',index=False)
